80ea1accd12049b0f247145d4352c6f03d77bd87
younesZdDz <fy_zadi@esi.dz>
2021-07-27 23:08:15 +0200

    refact: refactored move method in LawnMower

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Grid } from './models/Grid';
 import { getGridFromInputFile } from './utils';
 
 export async function run() {
-    const fileName = './tests/test-a.txt'; //! to parameterize this value
+    const fileName = process.argv[2]; //! to parameterize this value
     const grid: Grid = await getGridFromInputFile(fileName);    
     grid.runLawnMowers();
     grid.printCurrentState();
--- src/models/LawnMower.ts
+++ src/models/LawnMower.ts
@@ -15,71 +15,60 @@ export class LawnMower {
 
     move(leftEdgeIndex: number, bottomEdgeIndex: number, rigthEdgeIndex: number, topEdgeIndex: number) {
         this.instructions.forEach(instruction => {
-            if(this.orientation === 'E') {
-                switch(instruction) {
-                    case  'F': {
-                        this.positionX =  Math.min(this.positionX + 1, rigthEdgeIndex); 
-                        break;
+            switch(instruction) {
+                case  'F': {
+                    switch(this.orientation) {
+                        case 'N': {
+                            this.positionY =  Math.min(this.positionY + 1, topEdgeIndex); break;
+                        }
+                        case 'S': {
+                            this.positionY =  Math.max(this.positionY - 1, bottomEdgeIndex); break;
+                        }
+                        case 'E': {
+                            this.positionX =  Math.min(this.positionX + 1, rigthEdgeIndex); break;
+                        }
+                        case 'W': {
+                            this.positionX =  Math.max(this.positionX -1, leftEdgeIndex); break;
+                        }
                     }
-                    case 'L': {
-                        this.orientation  = 'N';
-                        break;
+                    break;
+                }
+                case 'L': {
+                    switch(this.orientation) {
+                        case 'N': {
+                            this.orientation  = 'W'; break;
+                        }
+                        case 'S': {
+                            this.orientation = 'E'; break;
+                        }
+                        case 'E': {
+                            this.orientation = 'N'; break;
+                        }
+                        case 'W': {
+                            this.orientation = 'S'; break;
+                        }
                     }
-                    case 'R': {
-                        this.orientation = 'S';
-                        break;
+                    break;
+                }
+                case 'R': {
+                    switch(this.orientation) {
+                        case 'N': {
+                            this.orientation = 'E'; break;
+                        }
+                        case 'S': {
+                            this.orientation = 'W'; break;
+                        }
+                        case 'E': {
+                            this.orientation = 'S'; break;
+                        }
+                        case 'W': {
+                            this.orientation = 'N'; break;
+                        }
                     }
+                    break;
                 }
             }
-            else if(this.orientation === 'N') {
-                switch(instruction) {
-                    case  'F': {
-                        this.positionY =  Math.min(this.positionY + 1, topEdgeIndex); 
-                        break;
-                    }
-                    case 'L': {
-                        this.orientation  = 'W';
-                        break;
-                    }
-                    case 'R': {
-                        this.orientation = 'E';
-                        break;
-                    }
-                }
-            }
-            else if(this.orientation === 'S') {
-                switch(instruction) {
-                    case  'F': {
-                        this.positionY =  Math.max(this.positionY - 1, bottomEdgeIndex); 
-                        break;
-                    }
-                    case 'L': {
-                        this.orientation  = 'E';
-                        break;
-                    }
-                    case 'R': {
-                        this.orientation = 'W';
-                        break;
-                    }
-                }
-            }
-            else if(this.orientation === 'W') {
-                switch(instruction) {
-                    case  'F': {
-                        this.positionX =  Math.max(this.positionX -1, leftEdgeIndex); 
-                        break;
-                    }
-                    case 'L': {
-                        this.orientation  = 'S';
-                        break;
-                    }
-                    case 'R': {
-                        this.orientation = 'N';
-                        break;
-                    }
-                }
-            }
-        })
+        });
     }
 }
 
